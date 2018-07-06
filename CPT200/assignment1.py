8d559b8964d19eeef69b3cd0d5020299fa18e873
Whil <ncsupiratecaptain@gmail.com>
2018-07-05 19:49:38 -0600

    Initial commit.

---
+++ CPT200/assignment1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 19:41:48 2018
+
+@author: Whil
+"""
+
+total_cost = int(input("Enter the total cost: $"))
+estimated_profit = total_cost * 0.19
+print("Estimated profit: $" + str(estimated_profit))
