febcc7d189f996445067f8ad4acd789101a5047a
Whil <ncsupiratecaptain@gmail.com>
2018-07-26 21:01:50 -0600

    refactoring assignment 4 into 3 lines.

--- CPT200/assignment4a.py
+++ CPT200/assignment4a.py
@@ -4,15 +4,16 @@ Created on Thu Jul 26 20:36:49 2018
 
 @author: Whil
 
-This program is designed to take in 20 numbers, then display the max, min and sum.
-I'll do 10.
+This program is takes in x number then display the max, min and sum.
+It asks for the user to input 20 numbers but enforcing this seems pointlessly long.
+
 Assuming all the input is numbers.
+
+Also the comments here are longer than the program. 
+I really wanted to sort the list comprehension but it returns none and can't be turned into a list in place :(.
 """
 
 # List comprehension with split()
 ints = [int(n) for n in (input("Enter 10 numbers separated by spaces: ")).split()]
 ints.sort()
-
-print("smallest number: {smallest}".format(smallest = ints[0]))
-print("largest number: %d" % (ints[-1]))
-print("Sum: %d" % sum(ints))
+print("smallest number: {smallest} \nlargest number: {largest} \nsum: {sum}".format(smallest = ints[0], largest = ints[-1], sum = sum(ints)))
