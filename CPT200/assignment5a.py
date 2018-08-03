585726647ade549237e1b75d8b5c351b162aa53e
Whil <ncsupiratecaptain@gmail.com>
2018-08-02 21:52:24 -0600

    Finishes assignment 5b / Final.

--- CPT200/assignment5a.py
+++ CPT200/assignment5a.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jul 31 20:08:11 2018
-
 @author: Whil
-
 Problem 5a
 Read in a file and check the scores in it against the correct scores.
+This could be customizeable by setting the student_scores as an argv variable.
+I have chosent to hard code it.
 """
+def import_scores():
+  file = open('student_scores.txt', 'r')
+  raw_scores = file.readlines()
+  file.close
+  return raw_scores
 
 def process_raw_scores(raw_scores):
   scores = []
@@ -17,20 +22,37 @@ def process_raw_scores(raw_scores):
 
 def grade_scores(answer_key, scores):
   assessment = []
-  for i in len(answer_key):
-      if answer_key[i] == scores[i]
-        assessment.append(true)
-    else
-      
+  for i in range(0,len(answer_key)):
+    if answer_key[i] == scores[i]:
+      assessment.append(True)
+    else:
+      assessment.append(False)
+  return assessment
 
+def calculate_percentage(assessment):
+  num_right = 0
+  for i in assessment:
+    if i == True:
+      num_right += 1
+  return float(num_right/len(assessment) * 100.0)
 
-answer_key = [B, D, A, A, B, A, B, A, C, D, B, C, D, A, D, C, C, B, D, A]
-scores = []
-right_wrong  = []
+def print_pass_fail(percentage):
+  if percentage >= 50 :
+    print("PASS - %.2f" % percentage)
+  else:
+    print("FAIL - %.2f" % percentage)
 
-file = open('student_scores.txt', 'r')
-raw_scores = file.readlines()
-file.close
+def print_details(assessment, scores, answer_key):
+  for i, value in enumerate(assessment):
+    if value == True:
+      print("{num}. {score} - Correct".format(score = scores[i], num = i + 1) )
+    else:
+      print("X. {score} - Correct answer: {correct_answer}".format(score = scores[i], correct_answer = answer_key[i]))
 
-scores = process_raw_scores(raw_scores)
+# Basic run
+answer_key = ['B', 'D', 'A', 'A', 'B', 'A', 'B', 'A', 'C', 'D', 'B', 'C', 'D', 'A', 'D', 'C', 'C', 'B', 'D', 'A']
+scores = process_raw_scores(import_scores())
 assessment = grade_scores(answer_key, scores)
+percentage = calculate_percentage(assessment)
+print_pass_fail(percentage)
+print_details(assessment, scores, answer_key)
---
+++ CPT200/assignment5b.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 12 21:19:56 2018
+
+@author: Whil
+
+This is the fourth assignment for the AU CPT200 course.
+It continues on the employee system.
+This time it wants us to again reformat the output of the names.
+It also wants a basic strict search on SSN.
+And update user with given SSN.
+I have omitted error checking on user input as I believe that is what 
+assignment 5 is all about.
+"""
+
+def prompt_user(all_employees):
+  choice = "0"
+  while choice not in ['1', '2', '3', '4', '5', '6', '7']:
+    print("There are currently {l} employees in the system.".format(l = len(all_employees)))
+    choice = input("Would you like to: \n1) enter a new user or 2)\nDisplay all users information\n3) Search for user by SSN\n4) Update a user\n5)export data\n6)import data\n7)exit: ")
+  return choice
+
+def get_employee_information():
+  employee = {}
+  employee['employeeName'] = input("Full name: ")
+  employee['employeeSSN_last4'] = input("Last 4 of SSN:" )
+  employee['employeePhone'] = input("Phone Number (###)###-#### format: ")
+  employee['employeeEmail'] = input("Email: ")
+  employee['employeeSalary'] = int(input("salary: "))
+  return employee
+
+def print_all_employees_information(all_employees):
+  print(" ")
+  for employee in all_employees:
+    print_basic_info(employee)
+  print(" ")
+
+def print_basic_info(employee):
+  num_spaces = 62
+  indent = 5
+  print_header(num_spaces, employee['employeeName'])
+  print_attribute(indent, "SSN:", employee['employeeSSN_last4'])
+  print_attribute(indent, "Phone:", employee['employeePhone'])
+  print_attribute(indent, "Email:", employee['employeeEmail'])
+  print_attribute(indent, "Salary: $", str(employee['employeeSalary']))
+  print_footer(num_spaces)
+  
+def print_header(num_spaces, employeeName):
+  tabs = (num_spaces - 2 - len(employeeName)) // 2
+  print(tabs * "-", end = '')
+  print(" " + employeeName + " ", end = '') 
+  print(tabs * "-")
+    
+def print_attribute(indent, attribute_name, attribute):
+  for i in range(0, indent):
+    print(" ", end = '')
+  print(attribute_name + "" + attribute)
+  
+def print_footer(num_spaces):
+  print(num_spaces * "-")
+  
+def find_employee_by_ssn(all_employees):
+  ssn = get_ssn()
+  index = get_employee_index(all_employees, ssn)
+  if index != -1 :
+    print_basic_info(all_employees[index])
+  else:
+    print("\nEmployee with SSN: %s not found.\n" % ssn)
+  return index
+
+def get_ssn():
+  ssn = input("Please enter the employee's ssn: ")
+  return ssn
+
+def get_employee_index(all_employees, ssn):
+  index = -1
+  for i, employee in enumerate(all_employees):
+    if ssn in employee.values():
+      index = i
+  return index
+  
+def update_employee(all_employees):
+  employee_index = find_employee_by_ssn(all_employees)
+  if employee_index != -1 :
+    print("\nEditing employee with SSN {ssn}:".format(ssn = all_employees[employee_index]['employeeSSN_last4']))
+    all_employees[employee_index] = get_employee_information()
+    
+def export_user_data(all_employees):
+  print("\nExporting Employees to employee_data.csv...")
+  with open('employee_data.csv', 'w') as file:
+    for employee in all_employees:
+      line = ""
+      for k, v in employee.items():
+        line += "{value},".format(value = v)
+      file.write(line[:-1]) # writes the line minus the last ','
+      file.write('\n')
+  print("Exported {n} employees.\n".format(n = len(all_employees)))
+
+def import_employee_data():
+  print("\nImporting employees...")
+  employees = []
+  with open('employee_data.csv', 'r') as file:
+    employees_data = file.readlines()
+    for employee_data in employees_data:
+      employee_data = employee_data.split(',')
+      employee = {}
+      employee['employeeName'] = employee_data[0]
+      employee['employeeSSN_last4'] = employee_data[1]
+      employee['employeePhone'] = employee_data[2]
+      employee['employeeEmail'] = employee_data[3]
+      employee['employeeSalary'] = int(employee_data[4])
+      employees.append(employee)
+  print("Employees imported.\n")
+  return employees
+
+# Variables
+all_employees = []
+choice = "0"
+
+while choice != "7":
+  choice = prompt_user(all_employees)
+  if choice == '1':
+    all_employees.append(get_employee_information())
+  elif choice == '2':
+    print_all_employees_information(all_employees)
+  elif choice =='3':
+    find_employee_by_ssn(all_employees)
+  elif choice == '4':
+    update_employee(all_employees)
+  elif choice == '5':
+    export_user_data(all_employees)
+  elif choice == '6':
+    all_employees += import_employee_data()
+  
+print("bye.")
